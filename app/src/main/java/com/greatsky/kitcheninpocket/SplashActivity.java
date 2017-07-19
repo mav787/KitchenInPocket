969f067aeab1f7774a9fb83fc363ac156c22da1a
mav787 <htsun1993@foxmail.com>
2017-07-19 06:18:36 -0400

    Update SplashActivity.java

--- app/src/main/java/com/greatsky/kitcheninpocket/SplashActivity.java
+++ app/src/main/java/com/greatsky/kitcheninpocket/SplashActivity.java
@@ -5,9 +5,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by fangwenli on 12/11/2016.
- */
 
 //the screen when you open the app, you can see the "Kitchen in Pocket" logo
 public class SplashActivity extends AppCompatActivity {
