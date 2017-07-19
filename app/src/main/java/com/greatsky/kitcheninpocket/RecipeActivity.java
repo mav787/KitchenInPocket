a9cfb16c26b332f6348e6572e5017062eb51b8cb
mav787 <htsun1993@foxmail.com>
2017-07-19 06:17:30 -0400

    Update RecipeActivity.java

--- app/src/main/java/com/greatsky/kitcheninpocket/RecipeActivity.java
+++ app/src/main/java/com/greatsky/kitcheninpocket/RecipeActivity.java
@@ -40,9 +40,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-/**
- * Created by fangwenli on 29/11/2016.
- */
+
 
 public class RecipeActivity extends Activity{
 
